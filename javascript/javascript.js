028cc7dd77037b36624dd3b124f823d76b26ab66
ottolevito <53096940+ottolevito@users.noreply.github.com>
2019-10-02 10:36:22 -0400

    Add files via upload

---
+++ javascript/javascript.js
@@ -0,0 +1,13 @@
+
+
+
+
+
+
+function printDOM(node, prefix) { console.log(prefix + node.nodeName);
+    for(let i=0; i<node.childNodes.length; i++) {
+                printDOM(node.childNodes[i], prefix + '\t');
+            }
+        }
+        printDOM(document, '');
+    
