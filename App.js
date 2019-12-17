e48521eabb9ac14f61f83844c4f89c6bbb7c9850
ottolevito <53096940+ottolevito@users.noreply.github.com>
2019-12-16 19:39:41 -0500

    Update App.js

--- App.js
+++ App.js
@@ -30,6 +30,31 @@ class App extends Component {
   }
 }
 
+body {
+  background-color: lightgreen;
+  margin: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.card {
+  display: flex;
+  background-color: #fff;
+  min-width: 100%;
+  min-height: 200px;
+  overflow-x: auto;}
+
+ .card::-webkit-scrollbar{
+   display: none;
+}
+
+.card--content {
+  background-color: #e74c3c;
+  min-width: 200px;
+  margin: 5px;
+}
+
   
 
 export default App
