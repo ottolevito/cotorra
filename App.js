261469ff04269c1a002c505121ea8c6407b85481
ottolevito <53096940+ottolevito@users.noreply.github.com>
2019-12-16 19:39:06 -0500

    Add files via upload
    css scroll horizontally

--- App.js
+++ App.js
@@ -1,36 +1,35 @@
-import React, { useState } from 'react';
+import React, { useState, Component } from 'react';
 import Person from './components/person'
-
-
-const App = props => {
-const [personsState, setPersonsState] = useState({
-  persons: [
-    {name:"David Palmolive", age:"18", deporte:"Baloncesto"},
-    {name:"Manuel Colgate ", age:"20",  deporte:"Beisbol"},
-    {name:"Braulio Listerine ", age:"19",  deporte:"Ajedrez"},
-  ],
-  otherState: 'some other value'
-});
-
-const switchNameHandler = () => {
-setPersonsState({
-    persons: [
-      {name:"Me dicen Deivi", age:"18", deporte:"Baloncesto"},
-    {name:"Me apodan Sonrisa ", age:"20",  deporte:"Beisbol"},
-    {name:"Me conocen como Buen Aliento ", age:"19",  deporte:"Ajedrez"},
-    ]
-  })
+import './App.css'
+class App extends Component {
+  render(){
+    return(<div>
+      <section className="card">
+      <div className="card--content"></div>
+      <div className="card--content"></div>
+      <div className="card--content"></div>
+      <div className="card--content"></div>
+      <div className="card--content"></div>
+      <div className="card--content"></div>
+      <div className="card--content"></div>
+      <div className="card--content"></div>
+      <div className="card--content"></div>
+      <div className="card--content"></div>
+      <div className="card--content"></div>
+      <div className="card--content"></div>
+      <div className="card--content"></div>
+      <div className="card--content"></div>
+      <div className="card--content"></div>
+      <div className="card--content"></div>
+      <div className="card--content"></div>
+      <div className="card--content"></div>
+      <div className="card--content"></div>
+      <div className="card--content"></div>
+    </section></div>
+    )
+  }
 }
 
-  return (
-    <div>
-      <button onClick={switchNameHandler}>Check me out</button>
- <h1>Los Deportistas del barrio</h1>
-      <Person name={personsState.persons[0].name} age={personsState.persons[0].age} deporte={personsState.persons[0].deporte}></Person>
-      <Person name={personsState.persons[1].name} age={personsState.persons[1].age}  deporte={personsState.persons[1].deporte}></Person>
-      <Person name={personsState.persons[2].name} age={personsState.persons[2].age}  deporte={personsState.persons[2].deporte}></Person>
-    </div>
-  );
-}
+  
 
-export default App;
+export default App
